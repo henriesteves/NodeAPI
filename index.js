5675c03a7581c8a52101ea62041e1c4f789b68e7
Henrique Esteves <henriesteves@gmail.com>
2018-02-24 17:31:04 -0300

    Add unique id for client object

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const uniqid = require('uniqid');
 
 const app = express();
 
@@ -7,32 +8,65 @@ app.use(bodyParser.json());
 
 let clients = [];
 
-app.post('/clients', (req, res) => {
-  clients.push(req.body);
+const getClientIndex = id => {
+  if (id) {
+    return clients.findIndex(client => client.id == id);
+  } else {
+    return -1;
+  }
+};
 
-  res.json({info: 'client create successfully'});
+app.post('/clients', (req, res) => {
+  let client = {};
+
+  client.id = uniqid();
+
+  client = Object.assign(client, req.body)
+
+  clients.push(client);
+
+  res.json([{info: 'client create successfully'}, {client: client}]);
 });
 
 app.get('/clients', (req, res) => {
   res.json(clients);
 });
 
-app.get('/clients/:name', (req, res) => {
-  res.json(clients.filter(client => client.name == req.params.name));
+app.get('/clients/:id', (req, res) => {
+  if (getClientIndex(req.params.id) >= 0) {
+    res.json(clients.filter(client => client.id === req.params.id));
+  } else {
+    res.json({info: 'client not found'});
+  }
 });
 
-app.put('/clients/:name', (req, res) => {
-  const clientIndex = clients.findIndex(client => client.name == req.params.name);
+app.put('/clients/:id', (req, res) => {
+  const clientIndex = getClientIndex(req.params.id);
 
-  clients[clientIndex] = req.body;
+  if (clientIndex >= 0) {
+    const clientId = clients[clientIndex].id;
+    let client = {};
 
-  res.json({info: 'client updated successfully'});
+    client.id = clients[clientIndex].id;
+
+    clients[clientIndex] = Object.assign(client, req.body)
+
+    res.json([{info: 'client updated successfully'}, {client: clients[clientIndex]}]);
+  } else {
+    res.json({info: 'client not found'});
+  }
 });
 
-app.delete('/clients/:name', (req, res) => {
-  clients = clients.filter(client => client.name != req.params.name);
+app.delete('/clients/:id', (req, res) => {
+  const clientIndex = getClientIndex(req.params.id);
 
-  res.json({info: 'client removed successfully'});
+  if (clientIndex >= 0) {
+    clients.splice(clientIndex, 1);
+    res.json({info: 'client removed successfully'});
+  } else {
+    res.json({info: 'client not found'});
+  }
+
 });
 
 app.listen(3000, () => {
@@ -48,8 +82,8 @@ app.listen(3000, () => {
 
 // curl -S -X GET http://localhost:3000/clients
 
-// curl -S -X GET http://localhost:3000/clients/Henrique
+// curl -S -X GET http://localhost:3000/clients/<id>
 
-// curl -S -H "Content-Type: application/json" -X PUT http://localhost:3000/clients/Henrique -d '{"name":"Henrique2","age": "31"}'
+// curl -S -H "Content-Type: application/json" -X PUT http://localhost:3000/clients/<id> -d '{"name":"Henrique2","age": "31"}'
 
-// curl -S -X DELETE http://localhost:3000/clients/Henrique
+// curl -S -X DELETE http://localhost:3000/clients/<id>
